274d2b7832e231771f8f61ee6fc5148f5e4baa95
surya-githubb <108220723+surya-githubb@users.noreply.github.com>
2025-07-09 12:37:13 +0530

    Create razorpayClient.js

---
+++ services/razorpay/src/utils/razorpayClient.js
@@ -0,0 +1,9 @@
+const Razorpay = require('razorpay');
+
+const razorpayClient = new Razorpay({
+  key_id: 'rzp_test_745X4T3Gos6sBu',
+  key_secret: 'XS0Pnl3Dbv8RgeLEYq3XKHBC',
+});
+
+
+exports.razorpayClient = razorpayClient;
