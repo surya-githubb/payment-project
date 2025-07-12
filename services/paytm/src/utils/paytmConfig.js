5e6dc668dcc8cd83613f9bfe647b00c8b40e8743
surya-githubb <108220723+surya-githubb@users.noreply.github.com>
2025-07-12 16:23:36 +0530

    Create paytmConfig.js

---
+++ services/paytm/src/utils/paytmConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mid: "DIY12386817555501617", // your MID
+  key: "bKMfNxPPf_QdZppa",      // your merchant key
+  website: "WEBSTAGING",
+  env: "https://securestage.paytmpayments.com", // or production URL
+};
