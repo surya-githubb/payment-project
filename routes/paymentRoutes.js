b50f75ae8eedb5b4b4818a6dca6a48aac53659ab
surya-githubb <108220723+surya-githubb@users.noreply.github.com>
2025-07-15 10:43:38 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const {initiatePayment} = require('../controllers/initiatePayment');
 const {paymentStatus} = require('../controllers/paymentStatus');
 
-router.post('/initiate', initiatePayment);
+router.post('/initiate', initiatePayment); // /initiate is same for all services
 
-router.post('/status/:orderId', paymentStatus);
+router.post('/status/:orderId', paymentStatus); // phonepe
 
-router.post('/status', paymentStatus);
+router.post('/status', paymentStatus); // razorpay
 
 
 module.exports = router;
